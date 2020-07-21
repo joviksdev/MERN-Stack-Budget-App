74668c0499d6f53f2a85d83dcbe42b9b8046bf8b
joviksdev <toch.emeo@gmail.com>
2020-07-21 01:13:08 +0100

    Prepare to push to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDb = require('./config/connectDb');
+const path = require('path');
 
 const app = express();
 
@@ -18,6 +19,13 @@ app.use('/api/register', require('./routes/register'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/budget', require('./routes/budget'));
 app.use('/api/expense', require('./routes/expense'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
